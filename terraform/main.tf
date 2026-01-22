64ad084293e7469559622f6a5b473f76bfa484ae
Funmi123 <ajanakueniola@gmail.com>
2026-01-22 17:41:46 +0100

    editted .yaml code and main.tf code

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,10 +28,10 @@ resource "aws_security_group" "allow_ssh_http" {
 }
 
 resource "aws_instance" "ansible" {
-  ami           = "ami-06f1fc9ae5ae7f31e"
-  instance_type = "c7i-flex.large"
-  key_name      = var.key_name
-  security_groups = [aws_security_group.allow_ssh_http.name]
+  ami                    = "ami-06f1fc9ae5ae7f31e"
+  instance_type          = "c7i-flex.large"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
 
   tags = {
     Name = "Ansible-Server"
@@ -39,10 +39,10 @@ resource "aws_instance" "ansible" {
 }
 
 resource "aws_instance" "java" {
-  ami           = "ami-06f1fc9ae5ae7f31e"
-  instance_type = "c7i-flex.large"
-  key_name      = var.key_name
-  security_groups = [aws_security_group.allow_ssh_http.name]
+  ami                    = "ami-06f1fc9ae5ae7f31e"
+  instance_type          = "c7i-flex.large"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
 
   tags = {
     Name = "Java-Node"
@@ -50,10 +50,10 @@ resource "aws_instance" "java" {
 }
 
 resource "aws_instance" "nginx" {
-  ami           = "ami-06f1fc9ae5ae7f31e"
-  instance_type = "c7i-flex.large"
-  key_name      = var.key_name
-  security_groups = [aws_security_group.allow_ssh_http.name]
+  ami                    = "ami-06f1fc9ae5ae7f31e"
+  instance_type          = "c7i-flex.large"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
 
   tags = {
     Name = "Nginx-Node"
